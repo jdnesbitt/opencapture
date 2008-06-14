69050f767bdda44236adf8e9f922eb5178cbdf94
jd_nesbitt@hotmail.com <jd_nesbitt@hotmail.com@4670e22a-fc48-0410-9fa4-47d024ee0b0f>
2008-06-14 19:43:11 +0000

    OpenCapture 0.0.1
        Stickey values not implemented.
    
    All other functionality is complete.

--- OpenCapture/0.0.1/src/net/filterlogic/OpenCapture/interfaces/IOCDeliveryPlugin.java
+++ OpenCapture/0.0.1/src/net/filterlogic/OpenCapture/interfaces/IOCDeliveryPlugin.java
@@ -1,7 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+Copyright 2008 Filter Logic
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 package net.filterlogic.OpenCapture.interfaces;
 
@@ -45,10 +56,10 @@ public interface IOCDeliveryPlugin
     /**
      * Deliver document is called for each document in the batch.  This method is called
      * once for each document in the batch.
-     * @param document Document to be delivered.
+     * @param document Configuration to be delivered.
      * @return Boolean set to true if document delivered successfully, else false.
      */
-    public boolean DeliverDocument(net.filterlogic.OpenCapture.Document document);
+    public boolean DeliverDocument(net.filterlogic.OpenCapture.Configuration document);
     
     /**
      * Closes the connection to the repository.  This method is called once when all documents have
